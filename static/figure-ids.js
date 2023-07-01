0645e51016a8cd8f5ee01468dbd8ee2e5115db37
tgwhi <tgwhi@pm.me>
2023-07-01 14:54:16 +1000

    Updated styling

--- static/figure-ids.js
+++ static/figure-ids.js
@@ -1,7 +1,14 @@
 let figures = document.getElementsByTagName('figure');
+let tables = document.getElementsByTagName('table');
 
 if (typeof(figures) != 'undefined' && figures != null) {
   for (let i = 0; i < figures.length; i++) {
-    figures[i].setAttribute('id', 'fig-' + (i + 1));
+    figures[i].setAttribute('id', 'figure-' + (i + 1));
+  }
+}
+
+if (typeof(tables) != 'undefined' && tables != null) {
+  for (let j = 0; j < tables.length; j++) {
+    tables[j].setAttribute('id', 'table-' + (j + 1));
   }
 }
