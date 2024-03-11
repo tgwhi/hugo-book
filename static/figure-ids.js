26d9e9239cfbbdbfd7e75c70a832689b97bfc668
Tim White <twhite49@myune.edu.au>
2024-03-11 11:14:15 +1100

    Add catppuccin-mocha theme

--- static/figure-ids.js
+++ static/figure-ids.js
@@ -1,5 +1,6 @@
 let figures = document.getElementsByTagName('figure');
 let tables = document.getElementsByTagName('table');
+let code = document.getElementsByClassName('chroma');
 
 if (typeof(figures) != 'undefined' && figures != null) {
   for (let i = 0; i < figures.length; i++) {
@@ -11,5 +12,11 @@ if (typeof(tables) != 'undefined' && tables != null) {
   for (let j = 0; j < tables.length; j++) {
     let currentId = tables[j].getAttribute('id');
     tables[j].setAttribute('id', currentId + ' table-' + (j + 1));
+  }
+}
+
+if (typeof(code) != 'undefined' && code != null) {
+  for (let i = 0; i < code.length; i++) {
+    code[i].setAttribute('id', 'code-' + (i + 1));
   }
 }
---
+++ static/owl-init.js
@@ -0,0 +1,8 @@
+$(document).ready(function(){
+  $(".owl-carousel").owlCarousel({
+    items: 1,
+    mouseDrag: false,
+    nav: true,
+    navText: ['<i class="fa-solid fa-angle-left" aria-hidden="true"></i>','<i class="fa-solid fa-angle-right" aria-hidden="true"></i>']
+  });
+});
